120c05c798326c968451a4b8074ed5133b83da49
6895mahfuzgit <6895mahfuz@gmail.com>
2021-03-20 11:37:48 +0600

    Test Worker Service to Get Product by Id and Add Product.

--- ProductWorkerServiceApp/Worker.cs
+++ ProductWorkerServiceApp/Worker.cs
@@ -1,5 +1,9 @@
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Net.Client;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using ProductGrpcMicroService.Protos;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,10 +15,12 @@ namespace ProductWorkerServiceApp
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private readonly IConfiguration _configuration;
 
-        public Worker(ILogger<Worker> logger)
+        public Worker(ILogger<Worker> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -22,8 +28,44 @@ namespace ProductWorkerServiceApp
             while (!stoppingToken.IsCancellationRequested)
             {
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
+
+                using var channel = GrpcChannel.ForAddress(_configuration.GetValue<string>("WorkerService:ServerUrl"));
+
+                var client = new ProductProtoService.ProductProtoServiceClient(channel);
+
+                await GetProdudtAsync(client);
+                await AddProductAsync(client);
+
+
+                await Task.Delay(_configuration.GetValue<int>("WorkerService:TaskInterval"), stoppingToken);
             }
+        }
+
+        private async Task AddProductAsync(ProductProtoService.ProductProtoServiceClient client)
+        {
+            var addProductResponse = await client.AddProductAsync(new AddProductRequest
+            {
+                Product = new ProductModel
+                {
+                    Name = "Test_2"+ DateTime.UtcNow,
+                    Description = "This is a test_2 product.",
+                    Price = 100,
+                    Status = ProductStatus.Instock,
+                    CreatedTime = Timestamp.FromDateTime(DateTime.UtcNow)
+                }
+            });
+
+            Console.WriteLine("Added Product:-" + addProductResponse.ToString());
+        }
+
+        private async Task GetProdudtAsync(ProductProtoService.ProductProtoServiceClient client)
+        {
+            var reply = await client.GetProductAsync(new GetProductRequest
+            {
+                ProductId = 1
+            });
+
+            Console.WriteLine(reply.ToString());
         }
     }
 }
