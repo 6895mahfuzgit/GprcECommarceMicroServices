ff23467ae7b4dffaba632e47b9948122e0b691a8
6895mahfuzgit <6895mahfuz@gmail.com>
2021-03-21 12:00:57 +0600

    Created Discount Project

---
+++ DiscountGrpcMicroServiceApp/Data/DiscountContext.cs
@@ -0,0 +1,13 @@
+﻿using DiscountGrpcMicroServiceApp.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiscountGrpcMicroServiceApp.Data
+{
+    public class DiscountContext : DbContext
+    {
+        protected DiscountContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Discount> Discounts { get; set; }
+    }
+}
---
+++ DiscountGrpcMicroServiceApp/Model/Discount.cs
@@ -0,0 +1,9 @@
+﻿namespace DiscountGrpcMicroServiceApp.Model
+{
+    public class Discount
+    {
+        public int DiscountId { get; set; }
+        public string Code { get; set; }
+        public int Amount { get; set; }
+    }
+}
